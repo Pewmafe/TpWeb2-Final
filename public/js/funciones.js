7831a36d517f21aa192ea01743ff708b0fd03f49
LancerFate <matias-leandro@outlook.com>
2020-11-21 21:01:15 -0300

    se agrego un boton modificar usuario y ya tiene funcionalidad con modal

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -17,3 +17,16 @@ $(document).on("click", ".botonDarDeBajaAcoplado", function () {
     var primaryKey = $(this).data('id');
     $(".modal-footer #botonDarDeBajaAcopladoModal").val( primaryKey );
 });
+
+$(document).on("click", ".botonModificarUsuario", function () {
+    var dni = $(this).data('dni');
+    var nombreUsuario = $(this).data('nombreusuario');
+    var nombre = $(this).data('nombre');
+    var apellido = $(this).data('apellido');
+    var fechanaci = $(this).data('fechanaci');
+    $(".formularioModificarUsuario .inputNombreUsuario").val( nombreUsuario );
+    $(".formularioModificarUsuario .inputNombre").val( nombre );
+    $(".formularioModificarUsuario .inputApellido").val( apellido );
+    $(".formularioModificarUsuario .inputDni").val( dni );
+    $(".formularioModificarUsuario .inputFechaNacimiento").val( fechanaci );
+});
--- view/administrarUsuariosView.php
+++ view/administrarUsuariosView.php
@@ -32,6 +32,9 @@
                                     <span class="h5 font-weight-bold">Fecha nacimiento</span>: {{fecha_nacimiento}}
                                 </p>
                                 <a href="registroEmpleado?nombreUsuario={{nombreUsuario}}" class="btn btn-primary">Hacerlo empleado</a>
+                                <a  class="mt-2 btn btn-success botonModificarUsuario" data-toggle="modal" data-target="#modificarUsuarioModal"
+                                    data-nombreusuario="{{nombreUsuario}}" data-nombre="{{nombre}}"
+                                    data-apellido="{{apellido}}" data-dni="{{dni}}" data-fechanaci="{{fecha_nacimiento}}" type="button">Modificar</a>
                                 <a type="button" class="btn btn-danger mt-2 botonDarDeBajaUsuario" data-toggle="modal" data-target="#darDeBajaUsuarioModal" data-id="'{{dni}}'">Dar de baja usuario</a>
                             </div>
                         </div>
@@ -111,6 +114,84 @@
                                 <button class="btn btn-danger" id="botonDarDeBajaEmpleadoModal" name="botonDarDeBajaEmpleadoModal">Baja</button>
                             </form>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="modificarUsuarioModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title text-dark" id="staticBackdropLabel">Modificar usuario</h5>
+                            <button type="button" class="close text-dark" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                            <div class="modal-body container">
+                                <form class="formularioModificarUsuario" method="POST"  action="">
+                                    <div class="form-group">
+                                        <label class="text-dark" for="">Nombre de usuario</label>
+                                        <input type="text" class="form-control inputNombreUsuario" id="" name="" >
+                                        <button class="btn btn-primary" id="" name="">Modificar</button>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="text-dark" for="">Nombre</label>
+                                        <input type="text" class="form-control inputNombre" id="" name="" >
+                                        <button class="btn btn-primary" id="" name="">Modificar</button>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="text-dark" for="">Apellido</label>
+                                        <input type="text" class="form-control inputApellido" id="" name="" >
+                                        <button class="btn btn-primary" id="" name="">Modificar</button>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="text-dark" for="">DNI</label>
+                                        <input type="text" class="form-control inputDni" id="" name="" >
+                                        <button class="btn btn-primary" id="" name="">Modificar</button>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="text-dark" for="">Fecha nacimiento</label>
+                                        <input type="date" class="form-control inputFechaNacimiento" id="" name="">
+                                        <button class="btn btn-primary" id="" name="">Modificar</button>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Volver</button>
+                                        <button type="submit" class="btn btn-primary" id="botonModificar" name="botonModificar">Modificar</button>
+                                    </div>
+                                </form>
+
+                                <!--<div class="form-group">
+                                    <label class="text-dark" for="nombreHeroe">Nombre del Heroe</label>
+                                    <input type="text" class="form-control" id="nombreHeroe" name="nombreHeroe" placeholder="Axe">
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label class="text-dark" for="atributoHeroe">Atributo principal</label>
+                                        <select class="form-control" id="atributoHeroe" name="atributoHeroe">
+                                            <option disabled selected>Tipo de Atributo</option>
+                                            <option value="Agilidad">Agilidad</option>
+                                            <option value="Fuerza">Fuerza</option>
+                                            <option value="Inteligencia">Inteligencia</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label class="text-dark" for="tipoAtaqueHeroe">Tipo de ataque</label>
+                                        <select class="form-control" id="tipoAtaqueHeroe" name="tipoAtaqueHeroe">
+                                            <option disabled selected>Tipo de Ataque</option>
+                                            <option>Cuerpo a cuerpo</option>
+                                            <option>Distancia</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="text-dark" for="historiaHeroe">Historia</label>
+                                    <textarea class="form-control" id="historiaHeroe" name="historiaHeroe" rows="8"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label class="text-dark" for="archivoImagen">Elegir imagen</label>
+                                    <input type="file" class="form-control-file text-light" id="archivoImagen" name="archivoImagen">
+                                </div>
+                            </div>
+                        </form>-->
                     </div>
                 </div>
             </div>
