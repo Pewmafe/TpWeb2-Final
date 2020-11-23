a61b8d38c14b0bdd8290be249f34c77c34d48a97
LancerFate <matias-leandro@outlook.com>
2020-11-22 22:15:36 -0300

    se creo la clase loginSession para ahorrar codigo

---
+++ helper/LoginSession.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class LoginSession
+{
+    public function verificarQueUsuarioEsteLogeado(){
+        $logeado = isset( $_SESSION["logeado"]) ?  $_SESSION["logeado"] : null;
+        if($logeado == 1){
+            return true;
+        }
+        return false;
+    }
+}
