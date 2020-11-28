ecf970f603e087c085c8e184e0d284285053e230
sanchez matias <matias-leandro@outlook.com>
2020-11-28 12:10:59 -0300

    se agrego todo el js dentro de un document.ready

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,113 +1,113 @@
-$(document).on("click", ".botonDarDeBajaUsuario", function () {
-    var primaryKey = $(this).data('id');
-    $(".modal-footer #botonDarDeBajaUsuarioModal").val(primaryKey);
-});
+$(document).ready(function (){
+    $(document).on("click", ".botonDarDeBajaUsuario", function () {
+        var primaryKey = $(this).data('id');
+        $(".modal-footer #botonDarDeBajaUsuarioModal").val(primaryKey);
+    });
 
-$(document).on("click", ".botonDarDeBajaEmpleado", function () {
-    var primaryKey = $(this).data('id');
-    $(".modal-footer #botonDarDeBajaEmpleadoModal").val(primaryKey);
-});
+    $(document).on("click", ".botonDarDeBajaEmpleado", function () {
+        var primaryKey = $(this).data('id');
+        $(".modal-footer #botonDarDeBajaEmpleadoModal").val(primaryKey);
+    });
 
-$(document).on("click", ".botonDarDeBajaCamion", function () {
-    var primaryKey = $(this).data('id');
-    $(".modal-footer #botonDarDeBajaCamionModal").val(primaryKey);
-});
+    $(document).on("click", ".botonDarDeBajaCamion", function () {
+        var primaryKey = $(this).data('id');
+        $(".modal-footer #botonDarDeBajaCamionModal").val(primaryKey);
+    });
 
-$(document).on("click", ".botonDarDeBajaAcoplado", function () {
-    var primaryKey = $(this).data('id');
-    $(".modal-footer #botonDarDeBajaAcopladoModal").val(primaryKey);
-});
+    $(document).on("click", ".botonDarDeBajaAcoplado", function () {
+        var primaryKey = $(this).data('id');
+        $(".modal-footer #botonDarDeBajaAcopladoModal").val(primaryKey);
+    });
 
-$(document).on("click", ".botonModificarUsuario", function () {
-    var dni = $(this).data('dni');
-    var nombreUsuario = $(this).data('nombreusuario');
-    var nombre = $(this).data('nombre');
-    var apellido = $(this).data('apellido');
-    var fechanaci = $(this).data('fechanaci');
+    $(document).on("click", ".botonModificarUsuario", function () {
+        var dni = $(this).data('dni');
+        var nombreUsuario = $(this).data('nombreusuario');
+        var nombre = $(this).data('nombre');
+        var apellido = $(this).data('apellido');
+        var fechanaci = $(this).data('fechanaci');
 
-    $(".formularioModificarUsuario .modal-footer #botonModificar").val(dni);
-    $(".formularioModificarUsuario .inputNombreUsuario").val(nombreUsuario);
-    $(".formularioModificarUsuario .inputNombre").val(nombre);
-    $(".formularioModificarUsuario .inputApellido").val(apellido);
-    $(".formularioModificarUsuario .inputDni").val(dni);
-    $(".formularioModificarUsuario .inputFechaNacimiento").val(fechanaci);
-});
+        $(".formularioModificarUsuario .modal-footer #botonModificar").val(dni);
+        $(".formularioModificarUsuario .inputNombreUsuario").val(nombreUsuario);
+        $(".formularioModificarUsuario .inputNombre").val(nombre);
+        $(".formularioModificarUsuario .inputApellido").val(apellido);
+        $(".formularioModificarUsuario .inputDni").val(dni);
+        $(".formularioModificarUsuario .inputFechaNacimiento").val(fechanaci);
+    });
 
-$(document).on("click", ".botonModificarEmpleado", function () {
-    var dni = $(this).data('dni');
-    var nombreUsuario = $(this).data('nombreusuario');
-    var nombre = $(this).data('nombre');
-    var apellido = $(this).data('apellido');
-    var fechanaci = $(this).data('fechanaci');
-    var tipoLicencia = $(this).data('tipolicencia');
-    var rol = $(this).data('rol');
-    var id = $(this).data('id');
+    $(document).on("click", ".botonModificarEmpleado", function () {
+        var dni = $(this).data('dni');
+        var nombreUsuario = $(this).data('nombreusuario');
+        var nombre = $(this).data('nombre');
+        var apellido = $(this).data('apellido');
+        var fechanaci = $(this).data('fechanaci');
+        var tipoLicencia = $(this).data('tipolicencia');
+        var rol = $(this).data('rol');
+        var id = $(this).data('id');
 
-    switch (rol) {
-        case 'administrador':
-            rol=1;
-            break;
-        case 'supervisor':
-            rol=2;
-            break;
-        case 'encargado':
-            rol=3;
-            break;
-        case 'chofer':
-            rol=4;
-            break;
-        case 'mecanico':
-            rol=5;
-            break;
-    }
+        switch (rol) {
+            case 'administrador':
+                rol=1;
+                break;
+            case 'supervisor':
+                rol=2;
+                break;
+            case 'encargado':
+                rol=3;
+                break;
+            case 'chofer':
+                rol=4;
+                break;
+            case 'mecanico':
+                rol=5;
+                break;
+        }
 
-    $(".formularioModificarEmpleado .modal-footer #botonModificar").val(dni);
-    $(".formularioModificarEmpleado .inputNombreUsuario").val(nombreUsuario);
-    $(".formularioModificarEmpleado .inputNombre").val(nombre);
-    $(".formularioModificarEmpleado .inputApellido").val(apellido);
-    $(".formularioModificarEmpleado .inputDni").val(dni);
-    $(".formularioModificarEmpleado .inputFechaNacimiento").val(fechanaci);
-    $(".formularioModificarEmpleado .selectTipoLicencia").val(tipoLicencia);
-    $(".formularioModificarEmpleado .selectRol").val(rol);
-    $(".formularioModificarEmpleado .modal-footer .idEmpleado").val(id);
-});
-
-
-$(document).on("click", ".botonModificarCamion", function () {
-    var patente = $(this).data('patente');
-    var nroChasis = $(this).data('nrochasis');
-    var nroMotor = $(this).data('nromotor');
-    var kilometraje = $(this).data('kilometraje');
-    var fabricacion = $(this).data('fabricacion');
-    var marca = $(this).data('marca');
-    var modelo = $(this).data('modelo');
-    var calendarioService = $(this).data('calendarioservice');
-
-    $(".formularioModificarCamion .modal-footer #botonModificarCamion").val(patente);
-    $(".formularioModificarCamion .inputNroChasis").val(nroChasis);
-    $(".formularioModificarCamion .inputKilometraje").val(kilometraje);
-    $(".formularioModificarCamion .inputNroMotor").val(nroMotor);
-    $(".formularioModificarCamion .inputPatente").val(patente);
-    $(".formularioModificarCamion .inputFabricacion").val(fabricacion);
-    $(".formularioModificarCamion .inputMarca").val(marca);
-    $(".formularioModificarCamion .inputModelo").val(modelo);
-    $(".formularioModificarCamion .inputCalendarioService").val(calendarioService);
-});
-
-$(document).on("click", ".botonModificarAcoplado", function () {
-    var patente = $(this).data('patente');
-    var chasis = $(this).data('chasis');
-    var tipoAcoplado = $(this).data('tipoAcoplado');
+        $(".formularioModificarEmpleado .modal-footer #botonModificar").val(dni);
+        $(".formularioModificarEmpleado .inputNombreUsuario").val(nombreUsuario);
+        $(".formularioModificarEmpleado .inputNombre").val(nombre);
+        $(".formularioModificarEmpleado .inputApellido").val(apellido);
+        $(".formularioModificarEmpleado .inputDni").val(dni);
+        $(".formularioModificarEmpleado .inputFechaNacimiento").val(fechanaci);
+        $(".formularioModificarEmpleado .selectTipoLicencia").val(tipoLicencia);
+        $(".formularioModificarEmpleado .selectRol").val(rol);
+        $(".formularioModificarEmpleado .modal-footer .idEmpleado").val(id);
+    });
 
 
-    $(".formularioModificarAcoplado .modal-footer #botonModificarAcoplado").val(patente);
-    $(".formularioModificarAcoplado .inputChasis").val(chasis);
-    $(".formularioModificarAcoplado .inputTipoAcoplado").val(tipoAcoplado);
-    $(".formularioModificarAcoplado .inputPatente").val(patente);
+    $(document).on("click", ".botonModificarCamion", function () {
+        var patente = $(this).data('patente');
+        var nroChasis = $(this).data('nrochasis');
+        var nroMotor = $(this).data('nromotor');
+        var kilometraje = $(this).data('kilometraje');
+        var fabricacion = $(this).data('fabricacion');
+        var marca = $(this).data('marca');
+        var modelo = $(this).data('modelo');
+        var calendarioService = $(this).data('calendarioservice');
 
-});
+        $(".formularioModificarCamion .modal-footer #botonModificarCamion").val(patente);
+        $(".formularioModificarCamion .inputNroChasis").val(nroChasis);
+        $(".formularioModificarCamion .inputKilometraje").val(kilometraje);
+        $(".formularioModificarCamion .inputNroMotor").val(nroMotor);
+        $(".formularioModificarCamion .inputPatente").val(patente);
+        $(".formularioModificarCamion .inputFabricacion").val(fabricacion);
+        $(".formularioModificarCamion .inputMarca").val(marca);
+        $(".formularioModificarCamion .inputModelo").val(modelo);
+        $(".formularioModificarCamion .inputCalendarioService").val(calendarioService);
+    });
 
-$(document).ready(function () {
+    $(document).on("click", ".botonModificarAcoplado", function () {
+        var patente = $(this).data('patente');
+        var chasis = $(this).data('chasis');
+        var tipoAcoplado = $(this).data('tipoAcoplado');
+
+
+        $(".formularioModificarAcoplado .modal-footer #botonModificarAcoplado").val(patente);
+        $(".formularioModificarAcoplado .inputChasis").val(chasis);
+        $(".formularioModificarAcoplado .inputTipoAcoplado").val(tipoAcoplado);
+        $(".formularioModificarAcoplado .inputPatente").val(patente);
+
+    });
+
     $('#Activos').click(function () {
         $('.popup1').show("slow");
         $('.popup2').hide(1000);
@@ -127,35 +127,36 @@ $(document).ready(function () {
         $('.popup2').hide(1000);
         $('.popup1').hide(1000);
     });
-});
 
 
-$(document).on("click", ".inputSiHazard", function () {
+    $(document).on("click", ".inputSiHazard", function () {
 
-    $("#hazard").collapse('show');
-});
+        $("#hazard").collapse('show');
+    });
 
-$(document).on("click", ".inputNoHazard", function () {
+    $(document).on("click", ".inputNoHazard", function () {
 
-    $("#hazard").collapse('hide');
-});
+        $("#hazard").collapse('hide');
+    });
 
-$(document).on("click", ".inputSiReefer", function () {
+    $(document).on("click", ".inputSiReefer", function () {
 
-    $("#reefer").collapse('show');
-});
+        $("#reefer").collapse('show');
+    });
 
-$(document).on("click", ".inputNoReefer", function () {
+    $(document).on("click", ".inputNoReefer", function () {
 
-    $("#reefer").collapse('hide');
-});
+        $("#reefer").collapse('hide');
+    });
 
-$(document).on("click", ".inputRegistrarCliente", function () {
+    $(document).on("click", ".inputRegistrarCliente", function () {
 
-    $("#registrarCliente").collapse('show');
-});
+        $("#registrarCliente").collapse('show');
+    });
 
-$(document).on("click", ".inputIngresarCuit", function () {
+    $(document).on("click", ".inputIngresarCuit", function () {
 
-    $("#registrarCliente").collapse('hide');
+        $("#registrarCliente").collapse('hide');
+    });
+
 });
