121bd7d3f21cc470d3a62aeb194d2970aab4e48d
sanchez matias <matias-leandro@outlook.com>
2020-12-14 17:09:31 -0300

    ahora aparecen los pedidos de mantenimiento a los mecanicos correspondientes

--- controller/MantenimientoController.php
+++ controller/MantenimientoController.php
@@ -20,11 +20,21 @@ class MantenimientoController
         $data["titulo"] = "mantenimiento";
         if ($logeado) {
             $data["login"] = true;
-
-            $tablaCamionesService = $this->mantenimientoModel->obtenerListaCamionesEnService();
-            $data['tablaCamionesService'] = $tablaCamionesService;
-
             $data2 = $this->loginSession->verificarQueUsuarioRol();
+            $usuarioAdmin=isset($data2["usuarioAdmin"]) ? isset($data2["usuarioAdmin"]) : null;
+            $usuarioMecanico = isset($data2["usuarioMecanico"]) ? isset($data2["usuarioMecanico"]) : null;
+
+            if($usuarioAdmin !=null){
+                $tablaCamionesService = $this->mantenimientoModel->obtenerListaCamionesEnService();
+                $data['tablaCamionesService'] = $tablaCamionesService;
+            }
+
+            if($usuarioAdmin==null and $usuarioMecanico!=null){
+                $tablaCamionesService = $this->mantenimientoModel->obtenerListaCamionesEnServicePorMecanicoId($_SESSION["idEmpleado"]);
+                $data['tablaCamionesService'] = $tablaCamionesService;
+            }
+
+
             $dataMerge = array_merge($data, $data2);
             echo $this->render->render("view/mantenimientoView.php", $dataMerge);
             exit();
